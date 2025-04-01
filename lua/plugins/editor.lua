dfdc7de8836fea276e1d1e68913f0898d7d404ee
Elite-zx <elite2022zx@gmail.com>
2025-04-01 22:41:11 +0800

    refine telescope config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -32,4 +32,5 @@ return {
 			keys = "etovxqpdygfblzhckisuran",
 		},
 	},
+
 }
--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -2,7 +2,14 @@
 -- Search everying in neovim
 -- ==============================
 
-local actions = require("telescope.actions")
+local ok_actions, actions = pcall(require, "telescope.actions")
+if not ok_actions then
+	actions = {
+		close = function() end,
+		to_fuzzy_refine = function() end,
+	}
+end
+
 local function fuzzy_grep()
 	require("telescope.builtin").grep_string({ search = "", only_sort_text = true })
 end
@@ -36,10 +43,10 @@ return {
 		},
 		keys = {
 			-- Find content with ripGrep among files under current directory
-			{ "<leader>fg", fuzzy_grep,                                desc = "fuzzy grep under cur dir" },
-			{ "<leader>fF", find_files_plus,                           desc = "Find file with specific dir" },
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>",           desc = "Find file under cur dir" },
-			{ "<leader>fr", "<cmd>Telescope git_files<cr>",            desc = "Find file under git repo" },
+			{ "<leader>fg", fuzzy_grep, desc = "fuzzy grep under cur dir" },
+			{ "<leader>fF", find_files_plus, desc = "Find file with specific dir" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file under cur dir" },
+			{ "<leader>fr", "<cmd>Telescope git_files<cr>", desc = "Find file under git repo" },
 			{ "<leader>fb", "<cmd>Telescope lsp_document_symbols<cr>", desc = "find tags in the current buffer" },
 
 			-- tags
